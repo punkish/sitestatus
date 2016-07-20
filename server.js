89326119fd3b25f6758fc61b1963ba0e3463092f
John J Czaplewski <jczaplew@gmail.com>
2016-07-20 09:10:09 -0500

    Add default bad status

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ function get(app, snap_id, callback) {
 
     if (error || response.statusCode != 200 || response.headers['content-length'] < 50) {
       status = -1
-      notifyFailure(app, response.statusCode)
+      notifyFailure(app, response.statusCode || 500)
     } else if (response.elapsedTime > (app.timeout || 1500)) {
       status = 0
       if (errors[app.name]) {
