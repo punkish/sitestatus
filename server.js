c8d06593e8d7dff2769dca8d8e3cd2df7a65ea46
punkish <pkishor@icloud.com>
2016-10-23 17:31:52 +0530

    better synthetic data generation, sorting of records before display

--- server.js
+++ server.js
@@ -4,8 +4,6 @@
 var express = require('express')
 var request = require('request')
 var config = require("./config");
-
-
 var Datastore = require('nedb')
 config.websites.forEach(function(app) {
     app.db = new Datastore({ filename: 'data/' + app.name + '.nedb', autoload: true })
@@ -15,23 +13,6 @@ var app = express()
 app.set('views', __dirname + '/templates')
 app.set('view engine', 'hjs')
 
-function notifyFailure(app, code) {
-    var msg = app.name + ' encountered a ' + code + ' status code when making a request to ' + app.uri
-    request({
-        uri: 'https://api.telegram.org/' + config.bot + '/sendMessage?chat_id=' + config.chat_id + '&text=' + msg
-    })
-}
-
-function notifyFix(app, status) {
-    errors[app.name] = false
-
-    var msg = app.name + ' is online' + ((status === 0) ? ', but is responding slowly' : ' and responding normally')
-
-    request({
-        uri: 'https://api.telegram.org/' + config.bot + '/sendMessage?chat_id=' + config.chat_id + '&text=' + msg
-    })
-}
-
 // Update a given website's status
 function get(website, callback) {
     request({
@@ -40,30 +21,31 @@ function get(website, callback) {
         timeout: config.timeout.split(/ /)[0] * 1000
     },
     function(error, response, body) {
-        var status = null;
 
-        if (error || response.statusCode != 200 || response.headers['content-length'] < 50) {
-            status = -1
-            var statusCode = (response && response.statusCode) ? response.statusCode : 500
-            notifyFailure(website, statusCode)
-        } 
-        else if (response.elapsedTime > config.timeout) {
-            status = 0
-            if (errors[website.name]) {
-                notifyFix(website, status)
-            }
-        } 
-        else {
-            status = 1
-            if (errors[website.name]) {
-                notifyFix(website, status)
-            }
-        }
         var response_time = (response && response.elapsedTime) ? response.elapsedTime : null
+        var status_code = (response && response.statusCode) ? response.statusCode : 500
+        var msg = website.uri;
 
+        if (error) {
+            msg += " caused the error: " + error;
+        }
+        else if (response.statusCode != 200 || response.headers['content-length'] < 50) {
+            msg += " is not up, responding with: " + response.statusCode;
+        }
+        else if (response.elapsedTime > config.timeout) {
+            msg += " is up but responding slowly"
+        }
+        else {
+            msg += " is working normally"
+        }
+
+        request({
+            uri: 'https://api.telegram.org/' + config.bot + '/sendMessage?chat_id=' + config.chat_id + '&text=' + msg
+        })
+            
         website.db.insert({
             ts: Date.now(),
-            status : status,
+            status : status_code,
             response_time : response_time
         })
     })
@@ -87,6 +69,7 @@ app.get('/', function(req, res, next) {
     var apps = []
     config.websites.forEach(function(website, cb) {
         website.db.find({})
+            .sort({ts : -1})
             .limit(req.query["limit"] || 10)
             .exec(
                 function (err, docs) {
@@ -98,15 +81,17 @@ app.get('/', function(req, res, next) {
 
                     // docs is [doc3, doc1]
                     docs.forEach(function(d) {
-                        d._class = d.status.toLowerCase()
 
                         if (d.response_time < 1000) {
+                            d.status = 'OK'
                             d.response_time = Math.floor(d.response_time) + ' ms'
                         }
                         else if (d.response_time >= 1000 && d.response_time < 60000) {
+                            d.status = 'SLOW'
                             d.response_time = Math.floor(d.response_time / 1000) + ' secs'
                         }
                         else {
+                            d.status = 'BAD'
                             d.response_time = '> ' + Math.floor(d.response_time / 60000) + ' mins'
                         }
 
--- syntheticdata.js
+++ syntheticdata.js
@@ -1,3 +1,5 @@
+var Datastore = require('nedb')
+
 var websites = [
     {
         name : 'TreatmentBank',
@@ -9,32 +11,45 @@ var websites = [
     }
 ]
 
-var Datastore = require('nedb')
-websites.forEach(function(app) {
-    var app = new Datastore({ filename: 'data/' + app.name + '.nedb', autoload: true })
+// See https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+var status_codes = {
+    "200" : "OKs",
+    "301" : "Moved Permanently",
+    "303" : "See Other",
+    "304" : "Not Modified",
+    "307" : "Temporary Redirect",
+    "400" : "Bad Request",
+    "401" : "Unauthorized",
+    "403" : "Forbidden",
+    "404" : "Not Found",
+    "405" : "Method Not Allowed",
+    "408" : "Request Timeout",
+    "500" : "Internal Server Error",
+    "502" : "Bad Gateway",
+    "503" : "Service Unavailable"
+}
+var codes = Object.keys(status_codes)
+var update_interval = '30 mins'
+var update_interval_ms = update_interval.split(/ /)[0] * 60000
+var count = 100
+var starting_ts = Date.now() - (count * update_interval.split(/ /)[0] * 60000)
 
-    for (var i=0, j=100; i<j; i++) {
-        var rand = Math.random() * (60000 - 100) + 100
+for (var i=0; i<websites.length; i++) {
+    console.log("inserting " + count + " records in " + websites[i].name + '.nedb')
+    var db = new Datastore({ filename: 'data/' + websites[i].name + '.nedb', autoload: true })
 
-        var status;
-        var perf = {
-            ok : 5000,
-            slow : 40000
-        }
-        if (rand < perf.ok) {
-            status = 'OK'
-        }
-        else if (rand >= perf.ok && rand < perf.slow) {
-            status = 'SLOW'
-        }
-        else {
-            status = 'BAD'
-        }
+    var docs = []
+    for (var j=0; j<count; j++) {
+        var idx = Math.floor(Math.random() * codes.length)
+        
+        docs.push({
+            ts: starting_ts + (j * update_interval_ms),
+            status_code : codes[idx],
 
-        app.insert({
-            ts: new Date(),
-            status : status,
-            response_time : rand
+            // A random response time between 100 ms and 1.5 mins
+            response_time : Math.random() * (90000 - 100) + 100
         })
     }
-})
+
+    db.insert(docs, function(err, docs) {})
+}
